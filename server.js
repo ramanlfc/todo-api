acc31f8becbafd54d4062e61b75011a64e2de9fa
raman <ramanlfc@gmail.com>
2017-09-30 11:06:08 +0530

    added GET /todos route

--- server.js
+++ server.js
@@ -5,13 +5,21 @@ const { Todo } = require('./model/todo.js');
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/todos', (req, res) => {
+    Todo.find({}).then(todos => {
+        res.send({ todos });
+    }).catch(e => {
+        res.status(500).send(e);
+    });
+});
+
 app.post('/todos', (req, res) => {
     var todo = new Todo(req.body);
 
-    todo.save().then(todo =>{
-       // console.log(todo);
+    todo.save().then(todo => {
+        // console.log(todo);
         res.send(todo);
-    },err=>{
+    }, err => {
         res.status(400).send(err);
     });
 });
